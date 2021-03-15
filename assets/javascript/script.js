f29146cd6c78a7d520e585c1995e065a13361946
Salome Ranson <salomeranson@gmail.com>
2021-03-15 19:23:00 -0400

    added event listeners for all save button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,17 @@
 
-var save1 = document.getElementById('save1');
+saveButton1 = document.getElementById('save8am');
+saveButton2 = document.getElementById('save9am');
+saveButton3 = document.getElementById('save10am');
+saveButton4 = document.getElementById('save11am');
+saveButton5 = document.getElementById('save12pm');
+saveButton6 = document.getElementById('save1pm');
+saveButton7 = document.getElementById('save2pm');
+saveButton8 = document.getElementById('save3pm');
+saveButton9 = document.getElementById('save3pm');
+saveButton10 = document.getElementById('save4pm');
+saveButton11 = document.getElementById('save5pm');
+
+
 
 
 
@@ -11,9 +23,50 @@ var now = moment().format('dddd MMMM Do YYYY');
 
 //FUNCTIONS
 //==========================================================
-$(save1).click(function() {
-    console.log('Save button was clicked!');
+$(saveButton1).click(function(e) {
+    console.log('Clicked!');
   });
+
+  $(saveButton2).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton3).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton4).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton5).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton6).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton7).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton8).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton9).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton10).click(function(e) {
+    console.log('Clicked!');
+  });
+
+  $(saveButton11).click(function(e) {
+    console.log('Clicked!');
+  });
+
 
 
 
