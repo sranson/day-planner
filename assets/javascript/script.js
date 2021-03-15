81ba890e02465e283981852532a1cd04966305a9
Salome Ranson <salomeranson@gmail.com>
2021-03-15 19:01:22 -0400

    added on click event listener to js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,22 @@
 
-var now = moment().format('dddd MMMM Do YYYY');
+var save1 = document.getElementById('save1');
 
-console.log(now);
+
+
+// VARIABLES
+//==========================================================
+var now = moment().format('dddd MMMM Do YYYY');
+//==========================================================
+
+
+//FUNCTIONS
+//==========================================================
+$(save1).click(function() {
+    console.log('Save button was clicked!');
+  });
+
+
+
+
+
+//==========================================================
