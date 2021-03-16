485452dba791a3bb0117aad5973b817b1dc422b1
Salome Ranson <salomeranson@gmail.com>
2021-03-15 22:32:28 -0400

    removed javascript code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,83 +1,21 @@
 
-text8am = document.getElementById('8am');
-saveButton1 = document.getElementById('save8am');
-Card8am = document.getElementById('Card8am');
-
-saveButton2 = document.getElementById('save9am');
-saveButton3 = document.getElementById('save10am');
-saveButton4 = document.getElementById('save11am');
-saveButton5 = document.getElementById('save12pm');
-saveButton6 = document.getElementById('save1pm');
-saveButton7 = document.getElementById('save2pm');
-saveButton8 = document.getElementById('save3pm');
-saveButton9 = document.getElementById('save3pm');
-saveButton10 = document.getElementById('save4pm');
-saveButton11 = document.getElementById('save5pm');
 
 
+// store all save buttons in a variable of some sort
+
+// add an event listener to this variable so that when a button is clicked, I can get
+// the id of the button that was clicked
+
+// when I know what button was clicked, I can add the text from that card to localStorage
+
+// once the text is added to localStorage, I can store it in a variable
+
+// Once the data from localStorage is stored in a variable, I can add this to the DOM
+// NOTE: I have to figure out how to keep this info in the DOM even when the page is refreshed
 
 
+// If a time block is in the past, make it [color]
 
-// VARIABLES
-//==========================================================
-var now = moment().format('dddd MMMM Do YYYY');
-//==========================================================
+// If a time block is the current time, make it [color]
 
-
-//FUNCTIONS
-//==========================================================
-$(saveButton1).click(function() {
-    text8am = text8am.value;
-    localStorage.setItem("8am", text8am);
-    var show8amContent = localStorage.getItem('8am');            //=> Grabs the value from localStorage -- is not populating in input box if page is refreshed
-    // show 'show8amContent on HTML page when refreshed
-    document.getElementById("Card8am").innerHTML = show8amContent;    // => Not showing when page is refreshed
-  
-  });
-
-  $(saveButton2).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton3).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton4).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton5).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton6).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton7).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton8).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton9).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton10).click(function() {
-    console.log('Clicked!');
-  });
-
-  $(saveButton11).click(function() {
-    console.log('Clicked!');
-  });
-
-
-
-
-
-
-//==========================================================
+// if the time block is in the future, make it [color]
