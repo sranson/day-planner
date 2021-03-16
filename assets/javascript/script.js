80c4885029f8256ca9b3a20992b47a32764387ea
Salome Ranson <salomeranson@gmail.com>
2021-03-15 20:08:23 -0400

    pulling data from localStorage and showing content on card- not displaying on page refresh

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
 
+text8am = document.getElementById('8am');
 saveButton1 = document.getElementById('save8am');
+Card8am = document.getElementById('Card8am');
+
 saveButton2 = document.getElementById('save9am');
 saveButton3 = document.getElementById('save10am');
 saveButton4 = document.getElementById('save11am');
@@ -23,47 +26,52 @@ var now = moment().format('dddd MMMM Do YYYY');
 
 //FUNCTIONS
 //==========================================================
-$(saveButton1).click(function(e) {
+$(saveButton1).click(function() {
+    text8am = text8am.value;
+    localStorage.setItem("8am", text8am);
+    var show8amContent = localStorage.getItem('8am');            //=> Grabs the value from localStorage -- is not populating in input box if page is refreshed
+    // show 'show8amContent on HTML page when refreshed
+    document.getElementById("Card8am").innerHTML = show8amContent;    // => Not showing when page is refreshed
+  
+  });
+
+  $(saveButton2).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton2).click(function(e) {
+  $(saveButton3).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton3).click(function(e) {
+  $(saveButton4).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton4).click(function(e) {
+  $(saveButton5).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton5).click(function(e) {
+  $(saveButton6).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton6).click(function(e) {
+  $(saveButton7).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton7).click(function(e) {
+  $(saveButton8).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton8).click(function(e) {
+  $(saveButton9).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton9).click(function(e) {
+  $(saveButton10).click(function() {
     console.log('Clicked!');
   });
 
-  $(saveButton10).click(function(e) {
-    console.log('Clicked!');
-  });
-
-  $(saveButton11).click(function(e) {
+  $(saveButton11).click(function() {
     console.log('Clicked!');
   });
 
