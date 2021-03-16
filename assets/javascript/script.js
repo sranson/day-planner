3679a713b7f5aca1eb5502348ea15e5a8a8d5554
Salome Ranson <salomeranson@gmail.com>
2021-03-15 23:57:07 -0400

    added comments to javascript file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,77 +12,79 @@ var card4pm = $('card4pm');
 var card5pm = $('card5pm');
 
 
+// add something (input?) so that the user can put text in the card body
+//When the user clicks "save" call the selectCard function
 
-// add an event listener to this variable so that when a button is clicked, I can get
-// the id of the button that was clicked
+
+
+
 $(buttons).click(function(e) {
     clickedBtn = e.target.id;
-    selectCard();
+    saveText();
 })
 
-// create a variable that easily links the targeted button to the corresponding card
-function selectCard() {
 
-     selectedCard = "";
+// create a variable that easily links the targeted button to the corresponding card
+function saveText() {
+
+     saveButton = "";
         switch(clickedBtn) {
             case 'saveBtn1':
                 console.log('Add text to 8am'); 
-                selectedCard= "card8am";
+                saveButton= "card8am";
                 break;
             case 'saveBtn2':
                 console.log('Add text to 9am');
-                selectedCard= "card9am";
+                saveButton= "card9am";
                 break;
             case 'saveBtn3':
                 console.log('Add text to 10am');
-                selectedCard= "card10am";
+                saveButton= "card10am";
                 break;
             case 'saveBtn4':
                 console.log('Add text to 11am');
-                selectedCard= "card11m";
+                saveButton= "card11m";
                 break;
             case 'saveBtn5':
                 console.log('Add text tp 12pm');
-                selectedCard= "card12pm";
+                saveButton= "card12pm";
                 break;
             case 'saveBtn6':
                 console.log('Add text to 1pm');
-                selectedCard= "card1pm";
+                saveButton= "card1pm";
                 break;
             case 'saveBtn7':
                 console.log('Add text to 2pm');
-                selectedCard= "card2pm";
+                saveButton= "card2pm";
                 break;
             case 'saveBtn8':
                 console.log('Add text to 3pm');
-                selectedCard= "card3pm";
+                saveButton= "card3pm";
                 break;
             case 'saveBtn9':
                 console.log('Add text to 4pm');
-                selectedCard= "card4pm";
+                saveButton= "card4pm";
                 break;
             case 'saveBtn10':
                 console.log('Add text to 5pm');
-                selectedCard= "card5pm";
+                saveButton= "card5pm";
                 break;
     }
-    console.log(selectedCard);
+    console.log(saveButton);
     updateCard();
 }
 
 function updateCard() {
-    if (selectedCard === 'card8am') {
-        // add something (input?) so that the user can put text in the card body
-        // store the data in a variable
+    if (saveButton === 'card8am') {
+        // store the input data in a variable
         // push the variable to localStorage
     }
 }
 
 
 
-// when I know what button was clicked, I can add the text from that card to localStorage
 
-// once the text is added to localStorage, I can store it in a variable
+
 
 // Once the data from localStorage is stored in a variable, I can add this to the DOM
 // NOTE: I have to figure out how to keep this info in the DOM even when the page is refreshed
